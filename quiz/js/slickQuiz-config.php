08c62112b65a8e0be4baf5fbe7a1e4f31e67154e
nowrap <nowrap@gmx.net>
2016-03-03 15:08:32 +0100

    quiz added

---
+++ quiz/js/slickQuiz-config.php
@@ -0,0 +1,96 @@
+<?php
+    $party = array(
+        "cdu"           =>  "CDU",
+        "spd"           =>  "SPD",
+        "gruene"        =>  "Bündnis 90 / Die Grünen",
+        "fw"            =>  "Freie Wähler",
+        "fdp"           =>  "FDP",
+        "linke"         =>  "Gießener Linke",
+        "buergerliste"  =>  "Bürgerliste Gießen",
+        "piraten"       =>  "Piratenpartei",
+        "afd"           =>  "Alternative für Deutschland",
+    );
+
+    $quiz = array(
+                array(
+                    "thema"     => "Wohnraum",
+                    "antworten" => array(
+                                    "cdu"           =>  "Wir setzen uns ein für mehr bezahlbaren Wohnraum, eine maßvolle Verdichtung der Innenstadt mit Erhalt von Grünzonen, besserem Schutz des Baumbestandes und ein intelligentes Parkraumkonzept. Wir wollen die behutsame Sanierung des Wohnungsbestandes der Wohnbau Gießen ohne Verdrängung der angestammten Mieterschaft.",
+                                    "spd"           =>  "Wir werden unter Zuhilfenahme aller Fördermöglichkeiten des Bundes und des Landes und mit mehr als 2 Millionen Eigenmitteln ein Gießener Investitionsprogramm für Sozialen Wohnungsbau auf den Weg bringen, mit dem in den kommenden Jahren Hunderte von Sozialwohnungen gebaut und bestehende Sozialbindungen erhalten werden.",
+                                    "gruene"        =>  "Barrierefreien bezahlbaren Wohnraum für ältere Menschen mit Fördermitteln von Bund und Land schaffen. Innenentwicklung vor Außenentwicklung ist ökologisch und ökonomisch sinnvoll, heißt weniger Flächenverbrauch, weniger Verkehr. Grünflächen für Freizeit und Naherholung erhalten und weiterentwickeln.",
+                                    "fw"            =>  "Durch die „Gleichberechtigung” des sozialen Wohnungsbaus neben der Schaffung von allgemeinem Wohnbedarf wollen wir erreichen, dass es keine Benachteiligung sozial Schwacher durch Luxussanierung gibt. Förderung des altersgerechten und betreuten Wohnens. Baugrundstücke für Familien sollen auch in Erbpacht vergeben werden.",
+                                    "fdp"           =>  "Wir treten für die Schaffung von erschwinglichem Wohnraum ein. Die Bereitstellung von Grundstücken für den Wohnungsbau genießt Vorrang, dabei sehen wir das Wachstum der Stadt westlich der Marburger Straße. Wir werden den Bedarf an freistehenden Einfamilienhäusern berücksichtigen.",
+                                    "linke"         =>  "Wir fordern ein Sofortprogramm, um mindestens 100 bezahlbare Sozialwohnungen jährlich zu schaffen, die Einführung einer verbindlichen Sozialquote von mindestens 10% bei privaten Bauprojekten und den Schutz von Mieterinnen und Mietern vor Verdrängung bei Sanierungen z.B. durch Milieuschutzklauseln.",
+                                    "buergerliste"  =>  "Gießen braucht eine verantwortungsvolle Stadtentwicklung, die dafür sorgt, dass zukünftig auch ausreichend bezahlbarer neuer Wohnraum entsteht und, dass die vorhandenen Sozialwohnungen auch bezahlbar bleiben. Wir setzen uns ein für einen Masterplan, der neben den sozialen Aspekten auch die umweltverträglichen Belange mit berücksichtigt.",
+                                    "piraten"       =>  "Es mangelt an Wohnraum, also wird Gießen wachsen müssen. Uns sind dabei Transparenz und Bürgerbeteiligung wichtig, um Probleme (z.B. Buslinie 13) zu vermeiden. Beim Ausbau müssen Soziales, Energetisches, Barrierefreiheit und Infrastruktur (z.B. Glasfaser, Leerrohre) stimmig sein.",
+                                    "afd"           =>  "Umsetzung des gemeinsamen Wohnraumplans mit dem Kreis . Stärkeres Engagement der Stadt. Erstellung eines Mietspiegels für Gießen ist notwendig. Demografischer Wandel des Umlandes muss durch Attraktivitätssteigerung gestoppt werden. Prüfen einer Sozialquote beim privaten Wohnungsbau zur Vermeidung von Ghettos.",
+
+                    ),
+                ),
+                array(
+                    "thema"     => "Finanzen",
+                    "antworten" => array(
+                                    "cdu"           =>  "Gießen ist dank der Unterstützung der Landesregierung auf einem guten Weg. Wir wollen aber auch Möglichkeiten nutzen, auf der Ausgabenseite der Stadt zu sparen und prüfen, ob ggf. Gebührenerhöhungen der gegenwärtigen Koalition rückgängig gemacht werden können.",
+                                    "spd"           =>  "Wir werden die Sanierung des städtischen Haushalts weiter vorantreiben und Schulden abbauen. Wir wollen möglichst schnell die Auflagen aus dem Schutzschirmvertrag erfüllen, um wieder frei und unabhängig Kommunalpolitik für Gießen gestalten zu können.",
+                                    "gruene"        =>  "Wir haben dieses Jahr einen ausgeglichenen Haushalt. Steuererhöhungen sind nicht vorgesehen, Gebühren werden an die Inflationsrate angepasst. Unsere soziale Infrastruktur werden wir erhalten. Bei stabilen Einkommen- und Gewerbesteuereinnahmen wollen wir aus dem Schutzschirmvertrag aussteigen.",
+                                    "fw"            =>  "Ständige Überprüfung von lnve~titionen auf ihre Folgekosten, um unliebsame Uberraschungen für den Haushalt zu vermeiden. Mehr Kooperation mit dem Kreis bei Abfallentsorgung und Volkshochschule. Umwandlung des Reinigungs- u. Fuhramtes in einen Eigenbetrieb zur Senkung der Kosten. Sozial verträglicher Schuldenabbau trotz Schutzschirm.",
+                                    "fdp"           =>  "Wir wollen mit nachhaltigen Finanzen für ein stabiles Wachstum und eine gesunde Stadt sorgen. Durch eine mittelfristige Senkung der Hebesätze bei der Gewerbesteuer ist ein erhöhtes Steueraufkommen zu erreichen. Die Erhöhung weiterer Steuern und Abgaben lehnen wir entschieden ab.",
+                                    "linke"         =>  "Wir fordern die Verwirklichung der hessischen Verfassung, die vorschreibt den Kommunen ausreichend Finanzmittel zur Verfügung zu stellen. Privatisierungen wie bei den Stadtwerken rückgängig machen. Einstellung der Zinszahlungen an die Banken. Wir bleiben dabei, Schuldenbremse heißt Sozial- und Demokratieabbau.",
+                                    "buergerliste"  =>  "Wir fordern einen Bürgerhaushalt, der die Vorschläge von Bürgern in Bezug auf Einsparungen mit berücksichtigt. Haushaltsausgaben sollten für notwendige und langlebige Projekte, z.B. den Straßenbau, investiert werden. Events, die den Steuerzahler in Gießen unnötig durch Steuererhöhungen belasten, wie die Landesgartenschau, sind zu vermeiden.",
+                                    "piraten"       =>  "Einnahmen rauf, Ausgaben runter. Schutzschirm einfahren. So einfach wird es wohl nicht! Mit StartUp-Szene, „CoworkingSpace”, „NearOffice” neue Jobs schaffen, um Menschen nach Ausbildung/Studium hier zu halten. Interkommunale Synergien nutzen. Einstieg in Wasserstoff, OpenSource und Bürgerhaushalte.",
+                                    "afd"           =>  "Nachhaltige, ausgeglichene Finanzpolitik ist anzustreben, die immer auch eine Schuldenreduzierung beinhaltet. Neue Schulden sind zu vermeiden, oder dürfen sich nur auf langfristige Investitionen beziehen. (Schulen) Schuldentilgung muss beschleunigt werden. Keine Subventionierung der Energiewende oder von ideologischen, defizitären Projekten.",
+                    ),
+                ),
+    );
+
+    shuffle($quiz);
+
+?>
+
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+var quizJSON = {
+    "info": {
+        "name":    "Kommunalwahl 2016 Quiz Gießen",
+        "main":    "<p>Sie kennen sich in der Stadtpolitik aus?<br />Dann testen Sie Ihr Wissen in 5 Themengebieten und 9 jeweils Positionen!</p>",
+        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
+        "level1":  "Jeopardy Ready",
+        "level2":  "Jeopardy Contender",
+        "level3":  "Jeopardy Amateur",
+        "level4":  "Jeopardy Newb",
+        "level5":  "Stay in school, kid..." // no comma here
+    },
+    "questions": [
+<?php
+    for ($i=0; $i<sizeof($quiz); $i++) {
+?>
+        { // Question <?php echo ($i+1); ?> - Multiple Choice, Single True Answer
+<?php
+        $pq = array_rand($quiz[$i]["antworten"]);
+
+        $q = $quiz[$i]["antworten"][$pq];
+?>
+            "q": "<?php echo $quiz[0]["thema"]; ?>:<br /><?php echo $q; ?>",
+            "a": [
+<?php
+        $c = 0;
+        foreach ($party as $p => $pname) {
+            $c++;
+?>
+                {"option": "<?php echo $pname; ?>",      "correct": <?php if ($p == $pq) echo "true"; else echo "false"; ?>}
+<?php
+            if ($c != sizeof($party)) echo ",\n"; else echo "\n";
+        }
+?>
+            ],
+            "correct": "<p><span>Absolut richtig</span> Sie haben die Position korrekt erkannt.</p>",
+            "incorrect": "<p><span>Leider falsch</span> Dies ist die Position von: <?php echo $party[$pq]; ?></p>" // no comma here
+        }
+<?php
+        if ($i != sizeof($quiz)) echo ","; else echo " // no comma here";
+   }
+?>
+    ]
+};
